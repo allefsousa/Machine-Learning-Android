682728ee0fca7edd35d914f6d2d92201ea3259c9
Allef Sousa <allefsousa_1@hotmail.com>
2017-06-20 20:10:12 -0300

    Aicompleta

--- app/src/main/java/com/developer/allef/machinelearningandroid/MainActivity.java
+++ app/src/main/java/com/developer/allef/machinelearningandroid/MainActivity.java
@@ -2,21 +2,67 @@ package com.developer.allef.machinelearningandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.JsonElement;
+
+import java.util.Map;
 
 import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
 import ai.api.model.AIError;
 import ai.api.model.AIResponse;
+import ai.api.model.Result;
 
 public class MainActivity extends AppCompatActivity implements AIListener{
+    private AIConfiguration config;
+    private AIService service;
+    private Button falar;
+    private TextView Status;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        falar = (Button)findViewById(R.id.button);
+        Status = (TextView)findViewById(R.id.textView);
+
+
+        config = new AIConfiguration("9845c17bfa80410f8ae387dff4b7730b",
+                AIConfiguration.SupportedLanguages.Portuguese,
+                AIConfiguration.RecognitionEngine.System);
+        service = AIService.getService(this, config);
+        service.setListener(this);
+
+        falar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                service.startListening();
+            }
+        });
     }
 
     @Override
     public void onResult(AIResponse result) { // é aqui onde você processa o resultado da requisição (a resposta)
+        Result results = result.getResult();
+
+        // Obter os parametros caso existam
+        String parameterString = "";
+        if (results.getParameters() != null && !results.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : results.getParameters().entrySet()) { // percorrendo o arquivo Json
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+            }
+        }
+
+        // Show results in TextView.
+        Status.setText("Query:" + results.getResolvedQuery() + //A frase que o utilizador usou
+                "\nSpeech: " + results.getFulfillment().getSpeech() + //A resposta
+                "\nParameters: " + parameterString); //Os parametros
 
     }
 
